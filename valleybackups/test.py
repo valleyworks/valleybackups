f97533287e6259abe7e49d45d3830fe0cd9e36ef
Nahuel Chaves <nahuel.chaves@gmail.com>
2016-04-02 23:31:10 -0300

    Some basic tests added

--- valleybackups/test.py
+++ valleybackups/test.py
@@ -2,22 +2,27 @@ import unittest
 from valleybackups.config_handler import ConfigurationHandler
 import os
 
+
 class ValleybackupsTest(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.config = ConfigurationHandler("test.conf")
 
     def test_open_config_file(self):
         """Test filesystem access to configuration file"""
         config = ConfigurationHandler("test.conf")
-        os.remove(os.path.join(os.path.dirname(__file__), "test.conf"))
         self.assertTrue(config)
 
-    def test_numbers_3_4(self):
-        self.assertEqual( 12, 12)
+    def test_set_valid_config(self):
+        self.config.set_config('base', 'aws_account_id', '123123123')
 
-    def test_strings_a_3(self):
-        self.assertEqual( 'aaa', 'aaa')
+
+    def test_set_invalid_config(self):
+        with self.assertRaises(Exception):
+            self.config.set_config('base', 'invalid_config', '123123123')
+
+    def tearDown(self):
+        os.remove(os.path.join(os.path.dirname(__file__), "test.conf"))
 
 if __name__ == '__main__':
     unittest.main()
